0e7a705114444da662bfce622759531645d75b5d
Alexey Yanov <a.d.katar@gmail.com>
2023-02-06 14:00:39 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package memory_cache
+
+import "fmt"
+
+func main() {
+	cache := cache.New()
+
+	cache.Set("userId", 42)
+	userId := cache.Get("userId")
+
+	fmt.Println(userId)
+
+	cache.Delete("userId")
+	userId := cache.Get("userId")
+
+	fmt.Println(userId)
+}
